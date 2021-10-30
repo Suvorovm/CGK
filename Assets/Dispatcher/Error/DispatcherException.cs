691ae457c1062096fb49b27c89f14aa86a16e757
Suvorov <Roym1ng58@yandex.ru>
2021-10-31 01:32:35 +0300

    feature(): add dispatcher

---
+++ Assets/Dispatcher/Error/DispatcherException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace CommonGameKit.CommonGameKit.Assets.Dispatcher.Error
+{
+    public class DispatcherException : Exception
+    {
+        public DispatcherException(string message) : base(message)
+        {
+        }
+    }
+}
---
+++ Assets/Dispatcher/Event/GameEvent.cs
@@ -0,0 +1,17 @@
+﻿namespace CommonGameKit.CommonGameKit.Assets.Dispatcher.Event
+{
+    public class GameEvent
+    {
+        private readonly string _eventName;
+
+        protected GameEvent(string eventName)
+        {
+            _eventName = eventName;
+        }
+
+        public string EventName
+        {
+            get { return _eventName; }
+        }
+    }
+}
