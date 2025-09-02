e3d097758bf50003c5543850b1d9b802bd718933
Suvorovm <maksimsuvorovru@gmail.com>
2025-09-02 15:02:39 +0300

    fix reference

--- CGK/Utils/LINQExtentions.cs
+++ CGK/Utils/LINQExtentions.cs
@@ -27,46 +27,6 @@ namespace CGK.Utils
             var list = enumerable as IList<T> ?? enumerable.ToList(); 
             return list.Count == 0 ? 0 : r.Next(0, list.Count);
         }
-        
-        /// <summary>
-        /// Performs an action on each element of a collection.
-        /// </summary>
-        public static IEnumerable<T> ForEach<T>(this IEnumerable<T> source, Action<T> action)
-        {
-            foreach (T element in source) action(element);
-            return source;
-        }
-
-        /// <summary>
-        /// Performs a function on each element of a collection.
-        /// </summary>
-        public static IEnumerable<T> ForEach<T, R>(this IEnumerable<T> source, Func<T, R> func)
-        {
-            foreach (T element in source) func(element);
-            return source;
-        }
-
-        /// <summary>
-        /// Performs an action on each element of a collection with its index
-        /// passed along.
-        /// </summary>
-        public static IEnumerable<T> ForEach<T>(this IEnumerable<T> source, Action<T, int> action)
-        {
-            int index = 0;
-            foreach (T element in source) { action(element, index); ++index; }
-            return source;
-        }
-
-        /// <summary>
-        /// Performs an action on each element of a collection with its index
-        /// passed along.
-        /// </summary>
-        public static IEnumerable<T> ForEach<T, R>(this IEnumerable<T> source, Func<T, int, R> func)
-        {
-            int index = 0;
-            foreach (T element in source) { func(element, index); ++index; }
-            return source;
-        }
 
     }
 }
